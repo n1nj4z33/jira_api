0668371e3234371aa10d3401c4b3adccee603d6e
Alexander Skiridomov <n1nj4z33@gmail.com>
2015-09-22 21:38:36 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+A python wrapper for the JIRA 6.x REST API
+For more information see https://www.atlassian.com/software/jira
+'''
+__author__ = 'Alexander Skiridomov'
+__contact__ = 'n1nj4z33@gmail.com'
+__date__ = '21.09.2015'
