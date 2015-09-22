6996c62fb0197bf5739872d84d968f70e5f8615b
Alexander Skiridomov <n1nj4z33@gmail.com>
2015-09-22 21:42:34 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-A python wrapper for the JIRA 6.x REST API
+A python wrapper for the Jira 6.x REST API
 For more information see https://www.atlassian.com/software/jira
 '''
 __author__ = 'Alexander Skiridomov'
